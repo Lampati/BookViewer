bd0596d8cccd5a0e303f51247b27ad80c105ab16
flanzani <flanzani@mobilecomputing.com.ar>
2018-03-06 11:23:11 -0300

    Repository base update

--- app/src/main/java/com/lampati/bookviewer/base/RepositoryBase.kt
+++ app/src/main/java/com/lampati/bookviewer/base/RepositoryBase.kt
@@ -1,8 +1,16 @@
 package com.lampati.bookviewer.base
 
+import com.lampati.bookviewer.R
 import com.lampati.bookviewer.commons.ResourcesService
 import com.lampati.bookviewer.commons.SharedPreferencesService
 import com.lampati.bookviewer.commons.ToastService
+import io.reactivex.Single
+import okhttp3.OkHttpClient
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+import java.net.HttpURLConnection
 
 /**
  * Created by FEDE on 6/3/2018.
@@ -10,4 +18,30 @@ import com.lampati.bookviewer.commons.ToastService
 abstract class RepositoryBase(protected val sharedPreferencesService: SharedPreferencesService,
                               protected val toastService: ToastService,
                               protected val resourcesService: ResourcesService) {
+
+
+    fun getRetrofit(): Retrofit {
+
+        return Retrofit.Builder()
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .addConverterFactory(GsonConverterFactory.create())
+                .baseUrl(" http://de-coding-test.s3.amazonaws.com")
+                .build()
+    }
+
+    fun <T> validateHttpResponse(reponse: Response<T>): Single<T> {
+
+        return Single.fromCallable {
+            if (reponse.isSuccessful) {
+                reponse.body()
+            } else {
+                when (reponse.code()) {
+                    HttpURLConnection.HTTP_NOT_FOUND -> throw Exception(resourcesService.getString(R.string.error_404))
+                    HttpURLConnection.HTTP_INTERNAL_ERROR -> throw Exception(resourcesService.getString(R.string.error_500))
+                    else -> throw Exception(resourcesService.getString(R.string.error_unknown))
+                }
+
+            }
+        }
+    }
 }
